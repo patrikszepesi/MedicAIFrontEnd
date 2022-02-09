79b41d01e1d2634c64c1dbe89f1690a37544ddee
patrikszepesi <patrikszepesi@gmail.com>
2022-02-09 13:08:06 +0100

    six

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js
-const withImages = require('next-images')
-module.exports = withImages()
-
-// module.exports = {
-//   env: {
-//   API='http://localhost:8000/api'
-//   },
-// }
